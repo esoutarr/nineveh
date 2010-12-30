d72f2ca18415666d941ea1225edc52097387ca07
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2010-12-30 17:10:11 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :development, :test do
+	gem 'jquery-rails'
+	gem 'sqlite3-ruby', :require => 'sqlite3'
+	gem 'capybara'
+	gem 'database_cleaner'
+	gem 'cucumber-rails'
+	gem 'cucumber'
+	gem "rspec-rails", ">= 2.0.1"
+	gem 'spork'
+	gem 'launchy'
+	gem 'ruby-debug19'
+	gem 'autotest'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nineveh::Application.config.secret_token = '26e91fa34423f370a25cbaffb3bd940fdfc056f8d81d139b15d54d75cfaa58320424542733f0baad04a307e28d9f8bc04fc41a4f9d7ccc27f80662d049c60936'
